314f06504f1a6ebb91caea5ad1ff948a47f07cd2
coloraddict <ajayvjaiswar@gmail.com>
2026-09-04 22:15:00 +0530

    feat(config) - add server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require ('mongoose');
+const dotenv = require ('dotenv');
+dotenv.config ({path: './.env'});
+
+const app = require ('./app');
+
+const DB = process.env.DATABASE.replace (
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect (DB)
+  .then (() => console.log ('DB Connection Successful'))
+  .catch (err => {
+    console.error ('DB Connection error: ', err.message);
+  });
+
+const port = process.env.PORT || 3000;
+app.listen (port, () => {
+  console.log (`App is listening on port ${port}`);
+});
