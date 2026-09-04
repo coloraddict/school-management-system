9aedf3cfbe1cd2f9e65e3b6fa0926c7cf3d22a1c
coloraddict <ajayvjaiswar@gmail.com>
2026-09-04 23:52:14 +0530

    feat(user): add controller and routes for adding a new user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,23 @@
+const User = require ('./../models/user.model');
+
 exports.getAllUsers = (req, res) => {
   res.status (200).json ({
     status: 'Success',
     message: 'Hello world! User route works',
   });
+};
+
+exports.addUser = async (req, res) => {
+  try {
+    const newUser = await User.create (req.body);
+    res.status (201).json ({
+      status: 'success',
+      data: newUser,
+    });
+  } catch (err) {
+    res.status (400).json ({
+      status: 'Failed',
+      message: err.message,
+    });
+  }
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,9 @@ const express = require ('express');
 const router = express.Router ();
 const userController = require ('./../controllers/user.controller');
 
-router.route ('/').get (userController.getAllUsers);
+router
+  .route ('/')
+  .get (userController.getAllUsers)
+  .post (userController.addUser);
 
 module.exports = router;
